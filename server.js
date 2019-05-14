f0fecd074effa6733b285905ccbe23b2700a4063
Srikar <srikarrampally@gmail.com>
2019-05-13 19:13:38 -0700

    update server file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/zipcode_auto' , function (req, res)
    res.setHeader("Content-Type","text/plain");
    res.setHeader("Access-Control-Allow-Origin","*");
    var parameters = url.parse(req.url, true).query;
-   var auto_zip_url = 'http://api.geonames.org/postalCodeSearchJSON?postalcode_startsWith='+parameters.zip+'&username=srikarrampally96&country=US&maxRows=5';
+   var auto_zip_url = 'http://api.geonames.org/postalCodeSearchJSON?postalcode_startsWith='+parameters.zip+'&username=[USERNAME]&country=US&maxRows=5';
    http.get(auto_zip_url,function(requ,resu)
    {
    	var results = "";
@@ -41,7 +41,7 @@ app.get('/searching' , function (req, res)
    res.setHeader("Access-Control-Allow-Origin","*");
    var parameters = url.parse(req.url, true).query;
    // console.log(parameters.Keyword);
-   var search_url = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=SrikarRa-srikarhw-PRD-816e08212-88cb6341&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=50&keywords='+encodeURI(parameters.Keyword)+'&buyerPostalCode='+parameters.buyerPostalCode;
+   var search_url = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=[APP-ID]&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=50&keywords='+encodeURI(parameters.Keyword)+'&buyerPostalCode='+parameters.buyerPostalCode;
    if(parameters.Category != "-1")
    {
       search_url += '&categoryId='+parameters.Category;
@@ -106,7 +106,7 @@ app.get('/shopping' , function (req, res)
    res.setHeader("Content-Type","text/plain");
    res.setHeader("Access-Control-Allow-Origin","*");
    var parameters = url.parse(req.url, true).query;
-   var shopping_url = 'http://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=SrikarRa-srikarhw-PRD-816e08212-88cb6341&siteid=0&version=967&ItemID='+parameters.itemId+'&IncludeSelector=Description,Details,ItemSpecifics';
+   var shopping_url = 'http://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=JSON&appid=[APP-ID]&siteid=0&version=967&ItemID='+parameters.itemId+'&IncludeSelector=Description,Details,ItemSpecifics';
    // console.log(shopping_url);
    http.get(shopping_url,function(requ,resu)
    {
@@ -131,7 +131,7 @@ app.get('/google_search' , function (req, res)
    res.setHeader("Content-Type","text/plain");
    res.setHeader("Access-Control-Allow-Origin","*");
    var parameters = url.parse(req.url, true).query;
-   var google_url = 'https://www.googleapis.com/customsearch/v1?q='+encodeURI(parameters.googlesearching)+'&cx=010618545303057637593:vvou7p-g_us&imgSize=huge&imgType=news&num=8&searchType=image&key=AIzaSyDicNVRiuP9Sr2LZj6Z4rKM1_4afF6qtTs';
+   var google_url = 'https://www.googleapis.com/customsearch/v1?q='+encodeURI(parameters.googlesearching)+'&cx=010618545303057637593:vvou7p-g_us&imgSize=huge&imgType=news&num=8&searchType=image&key=[APP-KEY]';
    console.log(google_url);
    https.get(google_url,function(requ,resu)
    {
@@ -155,7 +155,7 @@ app.get('/similarsearching' , function (req, res)
    res.setHeader("Content-Type","text/plain");
    res.setHeader("Access-Control-Allow-Origin","*");
    var parameters = url.parse(req.url, true).query;
-   var similar_url = 'http://svcs.ebay.com/MerchandisingService?OPERATION-NAME=getSimilarItems&SERVICE-NAME=MerchandisingService&SERVICE-VERSION=1.1.0&CONSUMER-ID=SrikarRa-srikarhw-PRD-816e08212-88cb6341&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&itemId='+parameters.itemids;
+   var similar_url = 'http://svcs.ebay.com/MerchandisingService?OPERATION-NAME=getSimilarItems&SERVICE-NAME=MerchandisingService&SERVICE-VERSION=1.1.0&CONSUMER-ID=[APP-ID]&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&itemId='+parameters.itemids;
    http.get(similar_url,function(requ,resu)
    {
       var results = "";
